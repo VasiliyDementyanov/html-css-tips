18aeb6c65800e5313a81d621ea1d282bdb1f73e0
dementr80 <dementr80@gmail.com>
2022-02-02 21:02:30 +0300

    pseudo classes code was added

---
+++ src/components/4_pseudoClasses/PseudoClasses.jsx
@@ -0,0 +1,29 @@
+import "./Background.css";
+
+// Block elements
+function PseudoClasses() {
+    return (
+		<><div className="block">
+			<p>
+				<a href="http://google.com" className="link">Lorem</a> ipsum dolor sit amet, consectetur adipisicing elit. Illo, eveniet. Quaerat molestiae veritatis beatae iure dolor, libero commodi ipsam dignissimos inventore, laborum vitae odio minima dolores hic, debitis corrupti asperiores?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur ab, a rem consequatur autem officiis eligendi. Aperiam ut voluptatum quia iure nam eaque, consequuntur magni et. Ullam magni, provident alias!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minima saepe, cum laboriosam molestiae ipsum maxime, reprehenderit quae sunt accusantium! Tempore dicta enim aut, distinctio praesentium libero animi ab eum?
+			</p>
+		</div><form action="#">
+				<input type="text" className="textinput" />
+				<button className="textbutton" type="submit">Отправить</button>
+			</form><ul className="list">
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+			</ul><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></>
+    );
+}
+
+export default PseudoClasses;
