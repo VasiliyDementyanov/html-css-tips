2bb9e5ec9536741ea2ef1a7fd8041d8e39366f16
dementr80 <dementr80@gmail.com>
2022-02-03 22:36:01 +0300

    before and after css pseudo classes were added

---
+++ src/components/5_BeforeAfter/BeforeAfter.jsx
@@ -0,0 +1,21 @@
+import "./Background.css";
+
+// Block elements
+function BeforeAfter() {
+    return (
+		<><p className="text">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolorem animi qui amet officiis debitis corrupti molestiae, adipisci nulla asperiores ut veritatis dolorum, ex unde reprehenderit doloremque eveniet ipsam libero.
+	</p>
+	<ul className="list">
+		<li>text</li>
+		<li>text</li>
+		<li>text</li>
+		<li>text</li>
+		<li>text</li>
+		<li>text</li>
+		<li>text</li>
+	</ul></>
+    );
+}
+
+export default BeforeAfter;
