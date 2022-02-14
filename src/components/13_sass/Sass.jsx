05b2ddb7a9bac07a23aaa5c6b8d44fc924822ea6
dementr80 <dementr80@gmail.com>
2022-02-14 21:13:44 +0300

    sass was added

---
+++ src/components/13_sass/Sass.jsx
@@ -0,0 +1,16 @@
+import "./Sass.css";
+
+// Block elements
+function Sass() {
+    return (
+		<>				<div className="block block--big">
+		<a href="" className="link">Google</a>
+		<div className="block__text">
+			Привет, я <span>фрилансер</span>!
+		</div>
+	</div>
+	<a href="" className="link-2">Google</a></>
+    );
+}
+
+export default Sass;
