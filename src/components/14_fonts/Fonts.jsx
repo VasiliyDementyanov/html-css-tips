57e42afd4193c9809e1b7de738ea938687887fc1
dementr80 <dementr80@gmail.com>
2022-02-15 21:48:40 +0300

    fonts lesson was added

---
+++ src/components/14_fonts/Fonts.jsx
@@ -0,0 +1,41 @@
+import "./Sass.css";
+
+// Block elements
+function Sass() {
+    return (
+		<>				<div className="textoriginal">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, adipisci ea, obcaecati nisi quia enim, doloribus non minima sunt voluptas distinctio magnam. Illo voluptas provident aliquid velit, dolorum fugit minus!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum assumenda eos aliquid, doloremque nulla totam quos sequi illum, perspiciatis magni minima, voluptate quis! Tempore aliquam itaque recusandae adipisci ab.
+		</p>
+	</div>
+	<div className="text">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, adipisci ea, obcaecati nisi quia enim, doloribus non minima sunt voluptas distinctio magnam. Illo voluptas provident aliquid velit, dolorum fugit minus!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum assumenda eos aliquid, doloremque nulla totam quos sequi illum, perspiciatis magni minima, voluptate quis! Tempore aliquam itaque recusandae adipisci ab.
+		</p>
+	</div>
+	<div className="text-PFDin">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, adipisci ea, obcaecati nisi quia enim, doloribus non minima sunt voluptas distinctio magnam. Illo voluptas provident aliquid velit, dolorum fugit minus!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum assumenda eos aliquid, doloremque nulla totam quos sequi illum, perspiciatis magni minima, voluptate quis! Tempore aliquam itaque recusandae adipisci ab.
+		</p>
+	</div>
+	<div className="text-PFDinMedium">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, adipisci ea, obcaecati nisi quia enim, doloribus non minima sunt voluptas distinctio magnam. Illo voluptas provident aliquid velit, dolorum fugit minus!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit laborum assumenda eos aliquid, doloremque nulla totam quos sequi illum, perspiciatis magni minima, voluptate quis! Tempore aliquam itaque recusandae adipisci ab.
+		</p>
+	</div></>
+    );
+}
+
+export default Sass;
